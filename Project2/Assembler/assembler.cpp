8e39980190d8445875eaa3929cc3cf671d16d593
Ömer Faruk Çakı <omerfarukcaki@gmail.com>
2020-09-06 23:40:19 +0300

    add AND

--- Project2/Assembler/assembler.cpp
+++ Project2/Assembler/assembler.cpp
@@ -196,6 +196,17 @@ main() {
                     counter++;
                 } else if (strcmp(token, "and") == 0) {
                     //to be added
+                     //to be added
+                    op1 = strtok(NULL, "\n\t\r ");
+                    op2 = strtok(NULL, "\n\t\r ");
+                    op3 = strtok(NULL, "\n\t\r ");
+                    // char op4[1];
+                    // op4[0] = '1';
+                    chch = (op1[0] - 48) | ((op2[0] - 48) << 3) | ((op3[0] - 48) << 6);
+                    program[counter] = 0x7000 + ((chch)&0x00ff) + 1024;
+                    cout << "AND op1: " << op1 << "   op2: " << op2 << "   op3: " << op3 << "   chch: " << chch << endl;
+                    printf("AND: %04x\n", program[counter]);
+                    counter++;
                 } else if (strcmp(token, "or") == 0) {
                     //to be added
                 } else if (strcmp(token, "xor") == 0) {
