0126c1faff5613e556ebb8de42ec9b7913e77b49
Cem Güleç <cem.ggulec@hotmail.com>
2020-09-06 22:40:57 +0300

    fix: total count

--- Project1/sseg_keypad/sseg_keypad.v
+++ Project1/sseg_keypad/sseg_keypad.v
@@ -14,6 +14,10 @@ reg [1:0] ready_buffer;
 reg ack;
 wire ready;
 
+reg [15:0] total;
+reg [1:0] state;
+reg [3:0] howmany_match;
+
 assign data=keyout;
 	
 sevensegment ss1 (.datain(data_all), .grounds(grounds), .display(display), .clk(clk));
@@ -26,16 +30,98 @@ always @(posedge clk)
 always @(posedge clk)
 	if (ready_buffer==2'b01)
 		begin
-			data_all<={data_all[11:0],keyout}; 
+			if(keyout == 4'hE || keyout == 4'hF)
+				data_all<= total;
+			else
+				
+				data_all<={data_all[11:0],keyout};
 			ack<=1;
 		end
 	else
 			ack<=0;
 
+always @(posedge clk)
+	begin
+		case(state)
+			2'b00:
+				begin
+					case(keyout)
+						4'hE: state <= 2'b01;
+						default:
+							begin
+								total <= data_all;
+								state <= 2'b00;
+							end
+					endcase
+				end
+			
+			2'b01:
+				begin
+					case(keyout)
+						4'h1:
+							begin
+								howmany_match <= 1;
+								state <= 2'b01;
+							end
+						4'h2:
+							begin
+								howmany_match <= 2;
+								state <= 2'b01;
+							end
+						4'h3:
+							begin
+								howmany_match <= 3;
+								state <= 2'b01;
+							end
+						4'hF:
+							begin
+								total <= total - howmany_match;
+								state <= 2'b10;
+							end
+					
+					endcase		
+				end
+				
+			2'b10:
+				begin
+					case(keyout)
+						4'h1:
+							begin
+								howmany_match <= 1;
+								state <= 2'b10;
+							end
+						4'h2:
+							begin
+								howmany_match <= 2;
+								state <= 2'b10;
+							end
+						4'h3:
+							begin
+								howmany_match <= 3;
+								state <= 2'b10;
+							end
+						4'hF:
+							begin
+								total <= total - howmany_match;
+								state <= 2'b01;
+							end
+					
+					endcase		
+				end
+				
+				
+				
+		
+		endcase
+	end
+			
+
 initial 	
 	begin
 		data_all=0;
 		ack=0;
+		state=2'b0;
+		total=1;
 	end
 
 endmodule
